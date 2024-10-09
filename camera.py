b5e9089c727e26c5d72eea4b2bc5753d9b7d9d52
Logan Hartford <logan.hartford@outlook.com>
2024-10-09 11:01:15 -0400

    basics led and gpio pin control

--- camera.py
+++ camera.py
@@ -1,21 +1,46 @@
 from RPi import GPIO
 from datetime import datetime
 import time
+import random
+
+def random_sign():
+    return random.choice([0, -1])
 
 GPIO.setmode(GPIO.BCM)
+
 button_pin = 17
+red_pin = 27
+green_pin = 22
 GPIO.setup(button_pin, GPIO.IN)
+GPIO.setup(red_pin, GPIO.OUT)
+GPIO.setup(green_pin, GPIO.OUT)
 
 
 def capture_image():
+    ret = random_sign()
+    if ret:
+        print("error")
+        return ret
     print("click")
+    return ret
 
 try:
-    print("Waiting for button press...")
+    for i in range(3):
+        GPIO.output(green_pin, GPIO.HIGH)
+        time.sleep(0.3)
+        GPIO.output(green_pin, GPIO.LOW)
+        time.sleep(0.3)
     while True:
         if (GPIO.input(button_pin) == GPIO.HIGH):
-            capture_image()
+            ret = capture_image()
+            if ret:
+                GPIO.output(red_pin, GPIO.HIGH)
+            else:
+                GPIO.output(green_pin, GPIO.HIGH)
             time.sleep(1)
+            GPIO.output(red_pin, GPIO.LOW)
+            GPIO.output(green_pin, GPIO.LOW)
+                
 
 except KeyboardInterrupt:
     pass
