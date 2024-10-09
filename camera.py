1fc32bd6f97e015e95f59b09d63aa67a0a930762
Logan Hartford <logan.hartford@outlook.com>
2024-10-09 10:43:14 -0400

    gpio working

--- camera.py
+++ camera.py
@@ -1,29 +1,21 @@
 from RPi import GPIO
-# from picamera2 import Picamera2
 from datetime import datetime
 import time
 
-# camera = Picamera2()
-
 GPIO.setmode(GPIO.BCM)
 button_pin = 17
-GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(button_pin, GPIO.IN)
 
-# camera.start()
 
 def capture_image():
-    # timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    # file_name = f"/home/pi/weeds/image_{timestamp}.jpg"
-    # camera.capture_file(file_name)
-    # print(f"Image captured: {file_name}")
     print("click")
 
 try:
     print("Waiting for button press...")
     while True:
-        if GPIO.input(button_pin) == GPIO.LOW:
+        if (GPIO.input(button_pin) == GPIO.HIGH):
             capture_image()
-            time.sleep(0.5)
+            time.sleep(1)
 
 except KeyboardInterrupt:
     pass
