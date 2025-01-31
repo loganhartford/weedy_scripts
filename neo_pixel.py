f3261696adda5c173d8f6b56ef8296a47852d610
loganhartford <logan.hartford@outloo.com>
2025-01-31 18:41:45 -0500

    testing

--- neo_pixel.py
+++ neo_pixel.py
@@ -1,7 +1,7 @@
 from pi5neo import Pi5Neo
 
 # Initialize the Pi5Neo class with 10 LEDs and an SPI speed of 800kHz
-neo = Pi5Neo('/dev/spidev0.0', 10, 800)
+neo = Pi5Neo('/dev/spidev0.0', 16, 800)
 
 # Fill the strip with a red color
 b = 1
